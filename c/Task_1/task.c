611fe142f685adc41a01422445ce5c23e5426e6f
rdm1234 <rdm1234@yandex.ru>
2017-09-29 21:56:30 +0300

    Update task.c

--- c/Task_1/task.c
+++ c/Task_1/task.c
@@ -13,25 +13,25 @@ int main(){
 
 	if (a != 0 && b != 0){
 		J = pow(10, -2)*a*b+(2*a/(b*g))+(3*b*g/a);
-		printf("J = %fn", J);
+		printf("J = %f\n", J);
 		if (d1 != 0)
 		{
 			K = (pow(10, -2)/(a+b))*(2/a+a/b-g/(b*d2));
-			printf("K = %fn", K);
+			printf("K = %f\n", K);
 		}
 		else
-			printf("Выражение K не имеет смыслаn");
+			printf("Выражение K не имеет смысла\n");
 	}
 	else
-		printf("Выражения J и K не имеют смыслаn");
+		printf("Выражения J и K не имеют смысла\n");
 	
 	if (d1 != -1 && d2 != -1)
 	{
 		L = a*b*g/(1+d1) + a*b*g/(1+d2) + 0.2;
-		printf("L = %fn", L);
+		printf("L = %f\n", L);
 	}
 	else
-		printf("Выражение L не имеет смыслаn");
+		printf("Выражение L не имеет смысла\n");
 	
 	system("pause");
 	system("cls");
