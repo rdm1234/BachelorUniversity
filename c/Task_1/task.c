50526c0ca0cb3efaaf3e01709b0657dac8277a82
rdm1234 <rdm1234@yandex.ru>
2017-09-29 21:18:45 +0300

    Update task.c

--- c/Task_1/task.c
+++ c/Task_1/task.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
 
 	int a, b, g = 0;
-	float d1, d2, L, K, S = 0;
+	float d1, d2, J, K, L = 0;
 	
 	while ((a == 0) || (b == 0) || (g == 0) || (d2 == 0)){
-		printf("Введите целые a, b, g, отличные от 0 и действиельные d1 и d2 не равное 0\n");
+		printf("Введите целые a, b, c, отличные от 0 и действиельные A и B не равное 0\n");
 		scanf("%d\n%d\n%d\n%f\n%f\n", &a, &b, &g, &d1, &d2);
 	}
 
-	L = 0.5*((a*b/g) + (b/(a*g)));
-	K = d1 + (((1/a)+(1/b)+(1/g))*d2*0.01);
-	S = 0.6 + (d1/d2*(a+b+g));
-	printf ("L = %f\nK = %f\nS = %f\n", L, K, S);
+	J = pow(10, -2)*a*b+(2*a/(b*g))+(3*b*g/a);
+	K = (pow(10, -2)/(a+b))*(2/a+a/b-g/(b*d2));
+	L = a*b*g/(1+d1) + a*b*g/(1+d2) + 0.2;
+	printf ("J = %f\nK = %f\nL = %f\n", J, K, L);
 	
-	system("pause");
-	system("cls");
 	return 0;
 }
