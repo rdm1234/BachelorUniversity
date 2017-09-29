98b7ba0c7b705e5b393b6aeb357e54dbf0eda339
rdm1234 <rdm1234@yandex.ru>
2017-09-29 21:53:45 +0300

    Update task.c

--- c/Task_1/task.c
+++ c/Task_1/task.c
@@ -1,6 +1,8 @@
 /* Вариант 15 */
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
 
 int main(){
 
@@ -11,26 +13,28 @@ int main(){
 
 	if (a != 0 && b != 0){
 		J = pow(10, -2)*a*b+(2*a/(b*g))+(3*b*g/a);
-		printf("J = %f\n", J);
+		printf("J = %fn", J);
 		if (d1 != 0)
 		{
 			K = (pow(10, -2)/(a+b))*(2/a+a/b-g/(b*d2));
-			printf("K = %f\n", K);
+			printf("K = %fn", K);
 		}
 		else
-			printf("Выражение K не имеет смысла\n");
+			printf("Выражение K не имеет смыслаn");
 	}
 	else
-		printf("Выражения J и K не имеют смысла\n");
+		printf("Выражения J и K не имеют смыслаn");
 	
 	if (d1 != -1 && d2 != -1)
 	{
 		L = a*b*g/(1+d1) + a*b*g/(1+d2) + 0.2;
-		printf("L = %f\n", L);
+		printf("L = %fn", L);
 	}
 	else
-		printf("Выражение L не имеет смысла\n");
+		printf("Выражение L не имеет смыслаn");
 	
+	system("pause");
+	system("cls");
 	
 	return 0;
 }
