e9434708f1a4af22b42bf2427fe65aba3c223f6c
rdm1234 <rdm1234@yandex.ru>
2018-03-20 11:34:10 +0300

    Create boolean10.c

---
+++ c/pract/boolean10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+  scanf("%d%d", &a,&b);
+  if((a%2==0 && b%2!=0) || (b%2==0 && a%2!=0))
+    printf("True\n");
+  else
+    printf("False\n");
+    
+  return 0;
+}
