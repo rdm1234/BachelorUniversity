7915924769957e8fdd4623cacbd89a6f9c2c80f9
rdm1234 <rdm1234@yandex.ru>
2018-03-20 11:38:49 +0300

    Create boolean25.c

---
+++ c/pract/boolean25.c
@@ -0,0 +1,14 @@
+// Рождественский boolean25
+#include <stdio.h>
+
+int main()
+{
+  int x,y;
+  scanf("%d%d", &x,&y);
+  if(x<0&&y>0)
+    printf("True\n");
+  else
+    printf("False\n");
+    
+  return 0;
+}
