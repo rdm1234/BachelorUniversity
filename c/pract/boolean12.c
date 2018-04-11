f37f9ecfaec4ea87cbe459ddf3ae3c445a382472
rdm1234 <rdm1234@yandex.ru>
2018-04-11 16:14:59 +0300

    Update boolean12.c

--- c/pract/boolean12.c
+++ c/pract/boolean12.c
@@ -1,11 +1,11 @@
-// Рождественский boolean12
+// Рождественский boolean13
 #include <stdio.h>
 
 int main()
 {
   int a,b,c;
   scanf("%d%d%d", &a,&b,&c);
-  if(a>&&b>0&&c>0)
+  if(a>0||b>0||c>0)
     printf("True\n");
   else
     printf("False\n");
