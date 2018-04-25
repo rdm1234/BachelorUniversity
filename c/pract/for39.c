8dc9c5dedd12ca6322fa84994e05d60468e09bd3
rdm1234 <rdm1234@yandex.ru>
2018-04-25 16:57:02 +0300

    Create for39.c

---
+++ c/pract/for39.c
@@ -0,0 +1,14 @@
+// Roghdestvensky for39
+#include <stdio.h>
+
+main()
+{
+  int A=10,B=15,i;
+  for(A; A<=B; A++)
+  {
+    for(i=0; i<A; i++)
+      printf("%d", A);
+    printf("\n");
+  }
+  return 0;
+}
