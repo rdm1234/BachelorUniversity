7673abadd3379c4c8bada9f112aca3cddacf5f1f
rdm1234 <rdm1234@yandex.ru>
2018-04-26 16:23:46 +0300

    Update 8.c

--- c/lections/8.c
+++ c/lections/8.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int pop(int *);
-void push(int *, int);
-void coutArr(int *);
+int pop(int *, int);
+void push(int *, int, int);
+void coutArr(int *, int);
 
 main(){
-  int *arr, arrSize,i;
+  int *arr, arrSize = 1,i;
   char c;
   arr = (int*)malloc(sizeof(int));
   arr[0]=100;
@@ -19,32 +19,35 @@ main(){
     if(c=='i'){
       printf("new num = ");
       scanf("%d", &i);
-      push(arr, i);
+      push(arr, i, arrSize);
+      arrSize++;
     }
-    if(c=='o')
-      printf("%d\n", pop(arr));
-    coutArr(arr);
+    if(c=='o'){
+      printf("%d\n", pop(arr, arrSize));
+      arrSize--;
+    }
+    coutArr(arr, arrSize);
     getchar();
   }
 
   return 0;  
 }
 
-int pop(int *arr){
-  if(sizeof(arr) == 0)
+int pop(int *arr, int s){
+  if(s == 0)
     return 0;
-  int num = arr[sizeof(arr)/sizeof(int*)-1];
+  int num = arr[s-1];
   
-  arr = (int*)realloc(arr, sizeof(arr)-sizeof(int*));
+  arr = (int*)realloc(arr, s*sizeof(int)-sizeof(int));
   return num;
 }
-void push(int *arr, int newNum){
-  arr = (int*)realloc(arr, sizeof(arr)+sizeof(int*));
-  arr[sizeof(arr)/sizeof(int*)-1] = newNum;
+void push(int *arr, int newNum, int s){
+  arr = (int*)realloc(arr, s*sizeof(int)+sizeof(int));
+  arr[s-1] = newNum;
 }
 
-void coutArr(int *arr){
-  int i, aS = sizeof(arr)/sizeof(int*);
-  for(i=0; i<aS; i++)
-    printf("%d\n", arr[i]);
+void coutArr(int *arr, int s){
+  int i;
+  for(i=0; i<s; i++)
+    printf("\n%d\n\n", arr[i]);
 }
