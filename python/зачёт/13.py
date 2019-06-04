a2f96defd58828b668a3b55d07065dc85859289d
rdm1234 <rdm1234@yandex.ru>
2019-06-04 17:01:31 +0300

    Update 13.py

--- python/зачёт/13.py
+++ python/зачёт/13.py
@@ -3,16 +3,17 @@ import random
 def task13(cards):
 	shake=random.randint(2,len(cards)-2)
 	part1=cards[:shake]
-	#print('part 1',part1)
+	print('Колода первая -',part1)
 	part2=cards[shake:]
-	#print('part 2', part2)
+	print('Колода вторая -',part2)
 	place = random.randint(0, len(part1) - 1)
-	#print('place', place)
+	print('place', place)
 	return part1[:place] + part2 + part1[place:]
 
 def test(n):
 	cards = [0,1,2,3,4,5,6,7,8,9,10]
-	print('Deck:', cards)
+	print('Колода:', cards)
 	for i in range(n):
 		cards = task13(cards)
-		print('Mixed', i+1, cards)
+		print('Перемешивание', i+1, cards)
+		print('---------------------------')
